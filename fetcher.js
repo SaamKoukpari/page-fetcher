d3ba9b5fe28264a6419f44e3550be0282b4b3969
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-23 11:23:57 -0700

    intial commit

---
+++ fetcher.js
@@ -0,0 +1,43 @@
+   
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+
+  input: process.stdin,
+  output: process.stdout
+
+});
+
+const getResource = function(serverAddress, localPath) {
+  request(serverAddress, (error, response, body) => {
+    if (error) {
+      console.log(error); 
+      process.exit();
+    }
+    fs.writeFile(localPath, body, (error) => {
+      fs.access(localPath, (error) => {
+        if (error) console.log(error);
+        process.exit();
+      });
+      if (error) console.log(error);
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+      rl.close();
+    });
+  });
+};
+
+const pageFetcher = function() {
+  const input = process.argv.slice(2);
+  const [server, path] = [input[0], input[1]];
+  if (fs.existsSync(path)) {
+    rl.question(`${path} already exists, do you want to confinue? (press y)`, (answer) => {
+      if (answer.toLowerCase() === 'y') {
+        getResource(server, path);
+      } else process.exit();
+    });
+    
+  } else getResource(server, path);
+};
+
+pageFetcher();
